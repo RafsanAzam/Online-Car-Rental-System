f54a1dfeccca7998528e7a066b24a2ae63216bd6
RafsanAzam <azam.rafsan@gmail.com>
2024-05-23 21:19:19 +1000

    Update IOrderService.cs

--- Online Car Rental System/Services/Interfaces/IOrderService.cs
+++ Online Car Rental System/Services/Interfaces/IOrderService.cs
@@ -2,6 +2,10 @@
 {
     public interface IOrderService
     {
+        //Method to create an order from a reservation ID
+        void CreateOrderFromReservation(int  reservationId);
+
+        //Method to confirm an order by its ID
         void ConfirmOrder(int id);
     }
 }
