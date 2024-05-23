5ffd2bbc9d03d929fa6fce86a2f96f75e8308c7f
RafsanAzam <azam.rafsan@gmail.com>
2024-05-24 09:26:31 +1000

    Update HomeController.cs

--- Online Car Rental System/Controllers/HomeController.cs
+++ Online Car Rental System/Controllers/HomeController.cs
@@ -1,21 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Online_Car_Rental_System.Models;
+using Online_Car_Rental_System.Services.Interfaces;
 using System.Diagnostics;
 
 namespace Online_Car_Rental_System.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
+        private readonly ICarService _carService;   
 
-        public HomeController(ILogger<HomeController> logger)
+      /*  public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+        } */
+
+        public HomeController(ICarService carService)
+        {
+            _carService = carService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var cars = _carService.GetAllCars();
+            return View(cars);
+        }
+
+        public IActionResult Search(string keyword)
+        {
+            var cars = _carService.SearchCars(keyword);
+            return View(cars);
+        }
+
+        public IActionResult Category(string category)
+        {
+            var cars = _carService.GetCarsByCategory(category);
+            return View(cars);
         }
 
         public IActionResult Privacy()
