b9ff0caba0a441c5b313c25e8c11aec8588da83a
RafsanAzam <azam.rafsan@gmail.com>
2024-05-23 19:02:11 +1000

    Create ReservationService.cs

---
+++ Online Car Rental System/Services/ReservationService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Online_Car_Rental_System.Data;
+using Online_Car_Rental_System.Models;
+using Online_Car_Rental_System.Services.Interfaces;
+
+namespace Online_Car_Rental_System.Services
+{
+    public class ReservationService : IReservationService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReservationService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Reservation> GetAllReservations()
+        {
+            return _context.Reservations.Include(r => r.Car).ToList();
+
+        }
+
+        public Reservation GetReservationById(int id)
+        {
+            return _context.Reservations.Include(r => r.Car).FirstOrDefault(r => r.ReservationId == id);
+        }
+
+        public void AddReservation(Reservation reservation)
+        {
+            _context.Reservations.Add(reservation);
+            _context.SaveChanges();
+        }
+
+        public void UpdateReservation(Reservation reservation)
+        {
+            _context.Reservations.Update(reservation);
+            _context.SaveChanges();
+        }
+
+        public void DeleteReservation(int id)
+        {
+            var reservation = _context.Reservations.FirstOrDefault(r => r.ReservationId == id);
+            if(reservation != null)
+            {
+                _context.Reservations.Remove(reservation);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
