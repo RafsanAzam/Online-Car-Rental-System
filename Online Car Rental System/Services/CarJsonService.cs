3aa3f35a0c53810faea3cbbbba19b08e189b8c3b
RafsanAzam <azam.rafsan@gmail.com>
2024-05-23 13:33:32 +1000

    Create CarJsonService.cs

---
+++ Online Car Rental System/Services/CarJsonService.cs
@@ -0,0 +1,17 @@
+﻿using Online_Car_Rental_System.Models;
+using System.Text.Json;
+
+namespace Online_Car_Rental_System.Services
+{
+    public class CarJsonService
+    {
+        private readonly string _filePath = "Online Car Rental System/cars.json";
+
+        public void UpdateJsonFile(List<Car> cars)
+        {
+            var jsonData = JsonSerializer.Serialize(cars, new JsonSerializerOptions { WriteIndented = true});
+            File.WriteAllText(_filePath, jsonData);
+        }
+
+    }
+}
