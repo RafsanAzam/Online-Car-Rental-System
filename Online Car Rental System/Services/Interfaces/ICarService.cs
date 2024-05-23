484a56bd975313da2da32aa49ebc6c525b58c24f
RafsanAzam <azam.rafsan@gmail.com>
2024-05-23 13:16:14 +1000

    Update ICarService.cs

--- Online Car Rental System/Services/Interfaces/ICarService.cs
+++ Online Car Rental System/Services/Interfaces/ICarService.cs
@@ -1,6 +1,17 @@
-﻿namespace Online_Car_Rental_System.Services.Interfaces
+﻿using Online_Car_Rental_System.Models;
+
+namespace Online_Car_Rental_System.Services.Interfaces
 {
     public interface ICarService
     {
+        List<Car> GetAllCars();
+        Car GetCarById(int id);
+        void AddCar(Car car);
+        void UpdateCar(Car car);
+        void DeleteCar(int id);
+        List<Car> SearchCars(string keyword);
+        List<Car> GetCarsByCategory(string category);
+        List<Car> GetCarsByBrand(string brand);
+
     }
 }
