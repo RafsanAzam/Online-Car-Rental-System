b45109493f972e31967eed7c0c6ac98064f8ceff
RafsanAzam <azam.rafsan@gmail.com>
2024-05-23 18:22:31 +1000

    Create IReservationService.cs

---
+++ Online Car Rental System/Services/Interfaces/IReservationService.cs
@@ -0,0 +1,14 @@
+﻿using Online_Car_Rental_System.Models;
+
+namespace Online_Car_Rental_System.Services.Interfaces
+{
+    public interface IReservationService
+    {
+        List<Reservation> GetAllReservations();
+        Reservation GetReservationById(int id);
+        void AddReservation(Reservation reservation);
+        void UpdateReservation(Reservation reservation);
+        void DeleteReservation(int id);
+
+    }
+}
