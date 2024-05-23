529b1940b8c697534d83245c84dd6087bfe6a5aa
RafsanAzam <azam.rafsan@gmail.com>
2024-05-23 14:49:53 +1000

    Create CarController.cs

---
+++ Online Car Rental System/Controllers/CarController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Online_Car_Rental_System.Models;
+using Online_Car_Rental_System.Services.Interfaces;
+
+namespace Online_Car_Rental_System.Controllers
+{
+    public class CarController : Controller
+    {
+        private readonly ICarService _carService;
+
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
+        public IActionResult Index()
+        {
+            var cars = _carService.GetAllCars();
+            return View(cars);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+       
+
+        [HttpPost]
+        public IActionResult Create(Car car)
+        {
+            if(ModelState.IsValid)
+            {
+                _carService.AddCar(car);
+                return RedirectToAction("Index");
+            }
+            return View(car);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var car = _carService.GetCarById(id);
+            if(car == null)
+            {
+                return NotFound();
+            }
+            return View(car);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                _carService.UpdateCar(car);
+                return RedirectToAction("Index");
+            }
+            return View(car);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _carService.DeleteCar(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
