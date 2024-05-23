09bad6533d878659b173e191ad412f6d3a52ac42
RafsanAzam <azam.rafsan@gmail.com>
2024-05-23 19:47:49 +1000

    Create ReservationController.cs

---
+++ Online Car Rental System/Controllers/ReservationController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Online_Car_Rental_System.Models;
+using Online_Car_Rental_System.Services;
+using Online_Car_Rental_System.Services.Interfaces;
+
+namespace Online_Car_Rental_System.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly IReservationService _reservationService;
+        private readonly ICarService _carService;
+
+        public ReservationController(IReservationService reservationService, ICarService carService)
+        {
+            _reservationService = reservationService;
+            _carService = carService;
+        }
+        
+        public IActionResult Create()
+        {
+            var cars = _carService.GetAllCars();
+            ViewBag.Cars = cars;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Reservation reservation)
+        {
+            if(ModelState.IsValid)
+            {
+                _reservationService.AddReservation(reservation);
+                return RedirectToAction("Details", new { id = reservation.ReservationId });
+            }
+            return View(reservation);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var reservation = _reservationService.GetReservationById(id);
+            if(reservation == null)
+            {
+                return NotFound();
+            }
+            var cars = _carService.GetAllCars();
+            ViewBag.Cars = cars;
+            return View(reservation);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Reservation reservation)
+        {
+            if(ModelState.IsValid)
+            {
+                _reservationService.UpdateReservation(reservation);
+                return RedirectToAction("Details", new {id = reservation.ReservationId});
+            }
+            return View(reservation);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var reservation = _reservationService.GetReservationById(id);
+            if(reservation == null)
+            {
+                return NotFound();
+            }
+            return View(reservation);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _reservationService.DeleteReservation(id);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
