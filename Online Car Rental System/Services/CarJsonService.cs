7b20f5c8813349eba3ae5c85751254e50e7d2906
RafsanAzam <azam.rafsan@gmail.com>
2024-05-24 11:59:50 +1000

    Update CarJsonService.cs
    added ReadJsonFile() Method.

--- Online Car Rental System/Services/CarJsonService.cs
+++ Online Car Rental System/Services/CarJsonService.cs
@@ -7,6 +7,17 @@ namespace Online_Car_Rental_System.Services
     {
         private readonly string _filePath = "Online Car Rental System/cars.json";
 
+        public List<Car> ReadJsonFile()
+        {
+            if(!File.Exists(_filePath))
+            {
+                throw new FileNotFoundException("The cars.json file was not found");
+            }
+
+            var jsonData = File.ReadAllText(_filePath);
+            return JsonSerializer.Deserialize<List<Car>>(jsonData);
+        }
+
         public void UpdateJsonFile(List<Car> cars)
         {
             var jsonData = JsonSerializer.Serialize(cars, new JsonSerializerOptions { WriteIndented = true});
