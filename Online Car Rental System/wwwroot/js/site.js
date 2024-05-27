4a977d79697331687cda6fd63f69ba61f1d4c348
RafsanAzam <azam.rafsan@gmail.com>
2024-05-27 11:46:07 +1000

    fixed image

--- Online Car Rental System/wwwroot/js/site.js
+++ Online Car Rental System/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    console.log("Script is running");
+    var gridView = $("#gridView");
+    var cardView = $("#cardView");
+    var toggleBtn = $("#toggleViewBtn");
+
+    toggleBtn.click(function () {
+        console.log("Button clicked");
+        if (gridView.hasClass("d-none")) {
+            gridView.removeClass("d-none");
+            cardView.addClass("d-none");
+            toggleBtn.text("Switch to Card View");
+        } else {
+            gridView.addClass("d-none");
+            cardView.removeClass("d-none");
+            toggleBtn.text("Switch to Grid View");
+        }
+    });
+});
