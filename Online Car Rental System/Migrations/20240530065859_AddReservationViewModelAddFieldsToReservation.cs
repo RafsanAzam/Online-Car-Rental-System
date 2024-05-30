6fda2f22c9687289ff731796bd190d9c1ce80e0e
RafsanAzam <azam.rafsan@gmail.com>
2024-05-30 17:12:43 +1000

    update reservation

---
+++ Online Car Rental System/Migrations/20240530065859_AddReservationViewModelAddFieldsToReservation.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Online_Car_Rental_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddReservationViewModelAddFieldsToReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasValidDriverLicense",
+                table: "Reservations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "MobileNumber",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Reservations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasValidDriverLicense",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "MobileNumber",
+                table: "Reservations");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Reservations");
+        }
+    }
+}
