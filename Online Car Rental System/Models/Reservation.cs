dca2b3343987206744bab224de90c870f0ac7442
RafsanAzam <azam.rafsan@gmail.com>
2024-05-30 16:50:24 +1000

    Update Reservation.cs

--- Online Car Rental System/Models/Reservation.cs
+++ Online Car Rental System/Models/Reservation.cs
@@ -8,10 +8,13 @@ namespace Online_Car_Rental_System.Models
         public int CarId { get; set; }
         public string Name { get; set; }
         public string UserEmail { get; set; }
+        public string MobileNumber { get; set; }
+        public bool HasValidDriverLicense { get; set; }
         public DateTime RentStartDate { get; set; }
         public DateTime RentEndDate { get; set; }
         public int Quantity { get; set; }
         public int TotalPrice { get; set; }
+        public string Status { get; set; } // New status field
 
         [ForeignKey("CarId")]
         public Car Car { get; set; }  // Navigation property
