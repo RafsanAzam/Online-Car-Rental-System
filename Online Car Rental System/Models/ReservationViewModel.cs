ac5d64179d864c2504ddc0082ce5a0766c758c94
RafsanAzam <azam.rafsan@gmail.com>
2024-05-30 16:56:36 +1000

    Create ReservationViewModel.cs

---
+++ Online Car Rental System/Models/ReservationViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Online_Car_Rental_System.Models
+{
+    public class ReservationViewModel
+    {
+        public int CarId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string UserEmail { get; set; }
+
+        [Required]
+        [Phone]
+        public string MobileNumber { get; set; }
+
+        [Required]
+        public bool HasValidDriverLicense { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime RentStartDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime RentEndDate { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Quantity must be at least 1.")]
+        public int Quantity { get; set; }
+
+        public int TotalPrice { get; set; }
+    }
+}
